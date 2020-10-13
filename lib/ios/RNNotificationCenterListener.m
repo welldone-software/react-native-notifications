30b9c9ce1259c82c9dc82bd2b78101237a98eccb
Atar Avraham <atara@welldone-software.com>
2020-10-13 15:48:45 +0300

    Removed saving notification from RNNotificationCenterListener.m

--- lib/ios/RNNotificationCenterListener.m
+++ lib/ios/RNNotificationCenterListener.m
@@ -1,25 +1,17 @@
 #import "RNNotificationCenterListener.h"
 #import "RCTConvert+RNNotifications.h"
-#import "RNNotificationsStorage.h"
-
-@import UIKit;
 
 @implementation RNNotificationCenterListener {
     RNNotificationEventHandler* _notificationEventHandler;
-    RNNotificationsStorage* _notificationStorage;
 }
 
 - (instancetype)initWithNotificationEventHandler:(RNNotificationEventHandler *)notificationEventHandler {
     self = [super init];
     _notificationEventHandler = notificationEventHandler;
-    _notificationStorage = [RNNotificationsStorage new];
     return self;
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-    if([[UIApplication sharedApplication] applicationState] != UIApplicationStateActive) {
-        [_notificationStorage saveNotification:notification];
-    }
     [_notificationEventHandler didReceiveForegroundNotification:notification withCompletionHandler:completionHandler];
 }
 
