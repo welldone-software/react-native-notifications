a44553efdacec91eb4dd2f2d62983117ec376588
Atar Avraham <atara@welldone-software.com>
2020-10-26 23:44:39 +0200

    Fixed unlock broadcast receiver

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/actions/NotificationActionService.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/actions/NotificationActionService.java
@@ -38,6 +38,7 @@ public class NotificationActionService extends HeadlessJsTaskService {
     private void promptUnlock() {
         Intent intent = new Intent(this, UnlockActivity.class);
         intent.setAction(UnlockActivity.PROMPT_UNLOCK_ACTION);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/actions/UnlockActivity.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/actions/UnlockActivity.java
@@ -1,47 +1,73 @@
 package com.wix.reactnativenotifications.core.actions;
 
 import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
-
-import com.wix.reactnativenotifications.R;
 
 public class UnlockActivity extends AppCompatActivity {
 
     public static final String PROMPT_UNLOCK_ACTION = "prompt_unlock";
 
+    private BroadcastReceiver mUnlockReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            KeyguardManager myKM = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            if (!myKM.inKeyguardRestrictedInputMode()) {
+                ActionPayloadSaver saver = ActionPayloadSaver.getInstance(context);
+                Bundle bundle = saver.getAwaitingAction();
+                if (bundle != null) {
+                    Intent serviceIntent = new Intent(context, NotificationActionService.class);
+                    serviceIntent.putExtras(bundle);
+                    context.startService(serviceIntent);
+                    NotificationActionService.acquireWakeLockNow(context);
+                }
+                saver.clearAwaitingAction();
+                UnlockActivity.this.finish();
+            }
+        }
+    };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_unlock);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_USER_PRESENT);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mUnlockReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onActionAppears(getIntent());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mUnlockReceiver);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    private void onActionAppears(Intent intent) {
         String action = intent.getAction();
         if (action != null && action.equals(PROMPT_UNLOCK_ACTION)) {
             KeyguardManager km = (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                km.requestDismissKeyguard(this, new KeyguardManager.KeyguardDismissCallback() {
-                    @Override
-                    public void onDismissError() {
-                        UnlockActivity.this.finish();
-                    }
-
-                    @Override
-                    public void onDismissSucceeded() {
-                        UnlockActivity.this.finish();
-                    }
-
-                    @Override
-                    public void onDismissCancelled() {
-                        UnlockActivity.this.finish();
-                    }
-                });
+                km.requestDismissKeyguard(this, null);
             } else {
                 getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
                 UnlockActivity.this.finish();
